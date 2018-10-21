73bc246b318810c0fd9af2dc69a8119f1549174d
gcruz@nnu.edu <genesis.cruz@allata.com>
2018-10-21 16:14:57 -0600

    updated data layer

--- CodeshopWarehouse.Data/OrderRepo.cs
+++ CodeshopWarehouse.Data/OrderRepo.cs
@@ -7,14 +7,37 @@ namespace CodeshopWarehouse.Data
 {
    public interface IOrderRepo {
         Order GetOrderById(int id);
-        List<Order> GetOpeOrders();
+        IEnumerable<Order> GetAllOpenOrders();
         void UpdateOrder(Order order);
+        void CreateOrder(Order order); //TODO: Keep in mind that you need to use a DTO
+        IEnumerable<Order> GetOrdersByProductId(int productId);
+
      }
     public class OrderRepo : IOrderRepo
     {
         private readonly IDbConnection _db;
         public OrderRepo(IDbConnection db) {
             _db = db;
+        }
+
+        public void CreateOrder(Order order) {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Order> GetAllOpenOrders() {
+            throw new NotImplementedException();
+        }
+
+        public Order GetOrderById(int id) {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Order> GetOrdersByProductId(int productId) {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrder(Order order) {
+            throw new NotImplementedException();
         }
     }
 }
