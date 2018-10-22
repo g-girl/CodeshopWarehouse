6e205c62a49bb2081141a1dce1c228894db1fa10
gcruz@nnu.edu <gcruz@nnu.com>
2018-10-21 18:04:23 -0600

    worked on business and api layers

---
+++ CodeshopWarehouse.Business/OrderService.cs
@@ -0,0 +1,47 @@
+﻿using CodeshopWarehouse.Data;
+using CodeshopWarehouse.Entities;
+using System;
+using System.Collections.Generic;
+
+
+namespace CodeshopWarehouse.Business
+{
+    public interface IOrderService {
+        Order GetOrderById(int id);
+        IEnumerable<Order> GetAllOpenOrders();
+        void UpdateOrder(Order order);
+        void CreateOrder(Order order); //TODO: Keep in mind that you need to use a DTO
+        IEnumerable<Order> GetOrdersByProductId(int productId);
+    }
+    public class OrderService : IOrderService {
+        private readonly IOrderRepo _orderRepo;
+
+        public OrderService(IOrderRepo orderRepo) {
+            _orderRepo = orderRepo;
+        }
+        public void CreateOrder(Order order) {
+             _orderRepo.CreateOrder(order);
+            
+        }
+
+        public IEnumerable<Order> GetAllOpenOrders() {
+           var orders = _orderRepo.GetAllOpenOrders();
+            return orders;
+        }
+
+        public Order GetOrderById(int id) {
+            var orderById = _orderRepo.GetOrderById(id);
+            return orderById;
+        }
+
+        public IEnumerable<Order> GetOrdersByProductId(int productId) {
+            var ordersByProductId = _orderRepo.GetOrdersByProductId(productId);
+            return ordersByProductId;
+        }
+
+        public void UpdateOrder(Order order) {
+            _orderRepo.UpdateOrder(order);
+            
+        }
+    }
+}
---
+++ CodeshopWarehouse.Web/Controllers/OrdersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeshopWarehouse.Business;
+using CodeshopWarehouse.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeshopWarehouse.Web.Controllers {
+    [Route("api/[controller]")]
+    public class OrdersController : Controller {
+        private readonly IOrderService _orderService;
+        public OrdersController(IOrderService orderService) {
+            _orderService = orderService;
+        }
+        // GET api/values
+        [HttpGet("{id}")]
+        public IActionResult GetOrderById(int id) {
+            return Ok(_orderService.GetOrderById(id));
+        }
+
+        [HttpGet]
+        public IActionResult GetAllOpenOrders() {
+            return Ok(_orderService.GetAllOpenOrders());
+        }
+
+        [HttpPut]
+        public IActionResult UpdateOrder([FromBody] Order order) {
+            _orderService.UpdateOrder(order);
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrder([FromBody] Order order) {
+            _orderService.CreateOrder(order);
+            return Ok();
+        } //TODO: Keep in mind that you need to use a DTO
+
+        [HttpGet ("byProductId/{productId}")]
+        public IActionResult GetOrdersByProductId(int productId) {
+            return Ok(_orderService.GetOrdersByProductId(productId));
+        }
+
+    }
+}
